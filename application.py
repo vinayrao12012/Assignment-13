516e73bfa415b2a1c486fd3aba98ce1fbf3c01fc
M Vinay Rao <78495014+vinayrao12012@users.noreply.github.com>
2023-09-01 17:34:14 +0530

    Update application.py

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ def details():
 			url = f'https://www.youtube.com/@{input_text}/videos'
 			headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
-			response = requests.get(url, headers=headers)
+			response = requests.get(url=url, headers=headers)
 			response_text = response.text
 
 			vid_titles = re.findall('"title":{"runs":\[{"text":".*?"', response_text)
